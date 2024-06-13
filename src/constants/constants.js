0692017dec0888df3d430f5e212e4da86eec2023
ZakariaAourir <zakaria1997aourir@gmail.com>
2024-06-13 16:45:43 +0200

    first commit: my portfolio

---
+++ src/constants/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Welcome to my workspace! I've been deeply immersed in JavaScript and TypeScript, crafting numerous front-end applications using robust frameworks like <b>Angular</b> and <b>React</b>. 
+      My journey in tech also extends to UI Design, where I've designed several intuitive interfaces for both mobile and web platforms. Additionally, I have a profound interest in AI and its transformative effects on the IT landscape—it's truly fascinating! No matter the language or the technology, my passion for programming remains undiminished.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk, and on it is my resume. <a href="https://github.com/ZakariaAourir/resume_eng/blob/main/cv-eng-AOURIR.pdf" target="_blank">Check it out?</a> My resume highlights my expertise in JavaScript, TypeScript, and UI Design, as well as my contributions to various projects. Contact me at <b>zakaria1997aourir@gmail.com</b> if you have any interesting job opportunities!`,
+    projects: `Here are some of the projects I've worked on: <br> <ui><li>Developed a pipeline tracking web app for ITP Interpipe</li><li>Enhanced UI consistency and usability for a leading tech company</li><li>Improved platform performance for Freterium's cloud supply chain platform</li></ui>
+    <li>Contributed to Sixense by crafting new UIs and maintaining best practices.</li><li>Worked on several personal design projects for mobile/web applications</li>`,
+    library: `This is my bookshelf, filled with all kinds of books in multiple languages including French, Arabic, English, and hopefully one day soon, Spanish. Although I haven't read all of them, I love collecting and referencing them for ideas and solutions. Some of my favorite genres include technology, science , and science fiction.`,
+    "cs-degree": `These are my degrees/certifications: <br> <ul><li>Software Engineering Degree at INPT - Rabat <a href="https://www.inpt.ac.ma" target="_blank">Check it out?</a></li><li>Udemy certificate in "The Complete Prompt Engineering for AI Bootcamp" <a href="https://www.udemy.com/course/prompt-engineering-for-ai/" target="_blank">Check it out?</a></li></ul> <br> I am always keen on learning new skills and staying updated with the latest in technology. Currently, I am exploring courses on advanced AI techniques, which is a follow up of my first AI course.
+`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube? twitch streams about AI and all sort of TV shows`,
+    "plant": `I like plants! They add a touch of nature to my workspace and help create a calming environment. Taking care of them is a nice break from coding :)`,
+    "plant2": `I like Plants :)`,
+    exit: `Thank you for checking out my portfolio! Feel free to reach out if you'd like to connect or have any opportunities to discuss. Have a great day! 🙂`
+  };
